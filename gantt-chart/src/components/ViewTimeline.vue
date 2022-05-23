0f3aa7419fd55fcda82664a0682b986b460472cc
Aditya Singh <201952202@iiitvadodara.ac.in>
2022-05-24 02:40:45 +0530

    Add section

--- gantt-chart/src/components/ViewTimeline.vue
+++ gantt-chart/src/components/ViewTimeline.vue
@@ -3,13 +3,23 @@
       <div class=" card-height border card1" align="left">
         
         <div  v-for="(section, index) in sections" :key="index" class="section">
-        <span>{{section}}</span>
+        <!-- <span>{{section}}</span> -->
+        <input type="text" v-model="section.value" class="sectionname" />
+        <div @click="edit(index)" class="dots"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+</svg>
+<span >
+</span>
+</div>
+
         </div>
-        <button>Add section</button>
+        <button class="button" @click="click()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus add" viewBox="0 0 18 18">
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg> Add section</button>
       </div>
       <div class=" card-height border card2" id="infinite" infinite-wrapper style=" overflow: auto;">
         <div class="timeline">
-          <li v-for="index in list" :key="index" style="display: inline; overflow-y: hidden; font-size: 0.8em" class="px-3">{{index}}</li>
+          <li v-for="(index,j) in list" :key="j" style="display: inline; overflow-y: hidden; font-size: 0.8em" class="px-3">{{index}}</li>
         </div>
         
         <div v-for="(list, index) in list1" :key="index" class="dragboard">
@@ -42,22 +52,35 @@ export default {
   name: "ViewTimeline",
   data() {
     return {
-      list1: [[{ name: "Drag Me!", section: 0}, {name: 'himi', section: 0}] ,  [{ name: "Drag Me Too!", section: 1}] , []],
+      list1: [[{ name: "Task 1", section: 0}, {name: 'Task 2', section: 0}] ,  [{ name: "Task 3", section: 1}] , []],
       list2: [],
       list: [],
-      sections: ['Section 1', 'Section 2', 'Section 3']
+      sections: [{value : 'Section 1'} , {value: 'Todo'} , {value: "Session"}]
     };
   },
   components: {
     draggable,
   },
   mounted() {
-        this.list =  [1,2,3,4,54,5,56,5,21,1,23,4,65,6,2,3,4,5,6,6,7,8,8,90,2,1,1,1,1,1,12,3];
+        this.list =  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
         let element = document.querySelector('#infinite');
         console.log(element)
         element.addEventListener("scroll", this.handleScroll);
     },  
   methods: {
+    edit(edit)
+    {
+      console.log(edit)
+    },
+    nameChange(i){
+      console.log(i)
+    },
+    click()
+    {
+    this.sections.push({value : 'Untitled Section'})
+    console.log(this.sections)
+    this.list1.push([])
+    },
     handleScroll() {
             let e = document.querySelector('#infinite');
             if(e.scrollLeft + e.clientWidth >= e.scrollWidth)
@@ -124,11 +147,13 @@ export default {
 {
   min-height: 50.5px;
   height: 50px;
-  padding: 5px 16px 0;
+  padding: 0px 16px 0;
   line-height: 20px;
   margin: 0;
   display: flex;
-  align-items: center;
+  align-items: center;  
+ transform: translateX(50px);
+
 }
 
 .dragline
@@ -200,5 +225,70 @@ border-bottom: 1px solid #e1e1e1;
 
 width: calc(100% - 38px);
 left: 0;
+}
+.sectionname{
+ border: none;
+ outline: none;
+ width: calc(100% - 80px);
+  cursor:pointer;
+  padding: 5px 5px;
+border-radius: 5px;
+ /* display: block; */
+}
+
+.sectionname:focus{
+  border: 1px solid #e1e1e1;
+  outline: 2px solid blue;
+  cursor:text
+
+}
+.button
+{
+  padding: 5px 10px 5px;
+  margin-top: 10px;
+ transform: translateX(56px);
+ background-color: white;
+ border: none;
+ border-radius: 5px;
+ color: rgb(131, 131, 131);
+  transition: all linear 0.2s;
+ 
+}
+.button:hover
+{
+  transition: all linear 0.2s;
+background-color: #ededed;
+color: rgb(0, 0, 0);
+}
+.button .add{
+  width: 0;
+  transform-origin: center center;
+  font-weight: 800;
+  transition: all ease 0.4s;
+
+
+}
+.button:hover .add 
+{
+width: 20px;
+transform-origin: center;
+  transition: all ease 0.4s;
+}
+
+
+.dots
+{
+  padding: 5px 8px;
+  color: rgb(110, 110, 110);
+  border-radius: 100px;
+  cursor: pointer;
+}
+
+.dots:hover
+{
+  background-image: radial-gradient(#ededed , white) ;
+  
+  box-shadow: 0px 0px 2px #ededed;
+  color: black;
 }
 </style>
